b28a57fec2623f10e60ecee49e404a33c183ca6e
Hassan Ahmad <ha1911307@gmail.com>
2023-12-22 02:37:14 -1200

    chai-aur-javascript

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,23 @@
+// prefer not to use var ,always use const and let
+
+
+// "use strict"; // treats all JS code as newer version
+
+// alert (3+3) we are using nodejs not the browser.
+
+
+// -------------------------------------DATA TYPE-----------------------------------------------------------
+
+let name = "husain"  // this is string datatype qouted with double quotation
+
+let age = 18 // this is integer datatype 
+
+let isLoggedIn = false // this is boolean datatype either true or false .
+
+// null => standalone value
+// undefined=> 
+// symbol => uniques
+// object 
+
+
+
